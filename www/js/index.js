58c0f8ffc502501a076dfdc0f651d36f01fc7c9f
Michael Vogt <michu@neophob.com>
2013-08-14 22:21:35 +0200

    tutorial 1 state

--- www/js/index.js
+++ www/js/index.js
@@ -34,16 +34,27 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        var myContact = navigator.contacts.create({"displayName": "Test User"});
+        myContact.note = "This contact has a note.";
+        console.log("The contact, " + myContact.displayName + ", note: " + myContact.note);
+        app.receivedEvent('contactcreated');
+	myContact.save(app.onContactSaveSuccess,app.onContactSaveError);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+    onContactSaveSuccess: function(contact) {
+        window.alert("Save Success");
+    },
+    onContactSaveError: function(contact) {
+	window.alert("Save Failed");
+    },
 };
+
