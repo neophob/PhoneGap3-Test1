3894e3654759158282c0499ec00b6d2d2f80588a
Michael Vogt <michu@neophob.com>
2013-08-15 22:28:53 +0200

    use The Revealing Module Pattern

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+//The Revealing Module Pattern
+var app = (function(){
     // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
+    function initialize() {
+        bindEvents();
+    }
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
+    function bindEvents() {
+        document.addEventListener('deviceready', onDeviceReady, false);
+    }
+
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    createMyUser: function(aNumber) {
+    function onDeviceReady() {
+        receivedEvent('deviceready');
+    }
+
+    function createMyUser(aNumber) {
         //hint, we're not looking for existing users, but create a new user each time!
         var myContact = navigator.contacts.create({"displayName": "Test User"+aNumber});
         myContact.note = "This contact has a note.";
+        console.log("try to save user");
+        myContact.save(onContactSaveSuccess, onContactSaveError);
         console.log("The contact, " + myContact.displayName + ", note: " + myContact.note);
-        myContact.save(app.onContactSaveSuccess,app.onContactSaveError);
-    },
+    }
+
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    function receivedEvent(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -51,12 +39,21 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-    onContactSaveSuccess: function(contact) {
-        showAlertMsg("Successfuly saved user "+contact.displayName);
-    },
-    onContactSaveError: function(contact) {
+    }
+
+    function onContactSaveSuccess(contact) {
+        showAlertMsg("Successfully saved user: "+contact.displayName);
+    }
+
+    function onContactSaveError(contact) {
 	   showAlertMsg("Save Failed");
-    },
-};
+    }
+
+
+return {
+  initialize:initialize,
+  createMyUser:createMyUser
+}
+
+}());
 
